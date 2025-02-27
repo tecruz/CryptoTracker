53fcb1565a57907e946fd7b4057f3834a1a180c0
tecruz <tiagex.cruz@gmail.com>
2025-02-27 23:23:15 +0000

    Add Kover

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kover)
 }
 
 
